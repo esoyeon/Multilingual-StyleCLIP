b53df361cecfd09de38de92d41e6e12985fcf9e6
esoyeon <nplsoyeon@gmail.com>
2021-12-03 12:20:08 +0900

    update optimizer

--- models/optimization/run_optimization.py
+++ models/optimization/run_optimization.py
@@ -1,3 +1,5 @@
+from utils import ensure_checkpoint_exists
+from mapper.training.train_utils import STYLESPACE_DIMENSIONS
 import argparse
 import math
 import os
@@ -24,6 +26,9 @@ def get_lr(t, initial_lr, rampdown=0.25, rampup=0.05):
     lr_ramp = lr_ramp * min(1, t / rampup)
 
     return initial_lr * lr_ramp
+
+
+# original code
 
 
 def main(args):
@@ -132,42 +137,3 @@ def main(args):
             final_result = img_gen
 
     return final_result
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--description", type=str, default="a person with purple hair",
-                        help="the text that guides the editing/generation")
-    parser.add_argument("--ckpt", type=str, default="../pretrained_models/stylegan2-ffhq-config-f.pt",
-                        help="pretrained StyleGAN2 weights")
-    parser.add_argument("--stylegan_size", type=int,
-                        default=1024, help="StyleGAN resolution")
-    parser.add_argument("--lr_rampup", type=float, default=0.05)
-    parser.add_argument("--lr", type=float, default=0.1)
-    parser.add_argument("--step", type=int, default=300,
-                        help="number of optimization steps")
-    parser.add_argument("--mode", type=str, default="edit", choices=[
-                        "edit", "free_generation"], help="choose between edit an image an generate a free one")
-    parser.add_argument("--l2_lambda", type=float, default=0.008,
-                        help="weight of the latent distance (used for editing only)")
-    parser.add_argument("--id_lambda", type=float, default=0.000,
-                        help="weight of id loss (used for editing only)")
-    parser.add_argument("--latent_path", type=str, default=None, help="starts the optimization from the given latent code if provided. Otherwose, starts from"
-                                                                      "the mean latent in a free generation, and from a random one in editing. "
-                                                                      "Expects a .pt format")
-    parser.add_argument("--truncation", type=float, default=0.7, help="used only for the initial latent vector, and only when a latent code path is"
-                                                                      "not provided")
-    parser.add_argument('--work_in_stylespace',
-                        default=False, action='store_true')
-    parser.add_argument("--save_intermediate_image_every", type=int, default=20,
-                        help="if > 0 then saves intermidate results during the optimization")
-    parser.add_argument("--results_dir", type=str, default="results")
-    parser.add_argument('--ir_se50_weights', default='../pretrained_models/model_ir_se50.pth', type=str,
-                        help="Path to facial recognition network used in ID loss")
-
-    args = parser.parse_args()
-
-    result_image = main(args)
-
-    torchvision.utils.save_image(result_image.detach().cpu(), os.path.join(
-        args.results_dir, "final_result.jpg"), normalize=True, scale_each=True, range=(-1, 1))
