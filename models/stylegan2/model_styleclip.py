a6d702ce0656956b623aa674cbe7ece2b125cc78
esoyeon <nplsoyeon@gmail.com>
2021-12-11 22:50:02 +0900

    fix minor error

--- models/stylegan2/model_styleclip.py
+++ models/stylegan2/model_styleclip.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from models.stylegan2.op_restyle import FusedLeakyReLU, fused_leaky_relu, upfirdn2d
+from models.stylegan2.op_styleclip import FusedLeakyReLU, fused_leaky_relu, upfirdn2d
 
 
 class PixelNorm(nn.Module):
